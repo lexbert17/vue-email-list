0e9c88ab7c4d8d9026ddf11a7e308a5eecce8e16
lexbert17 <lexbertlazo@gmail.com>
2023-11-21 20:42:42 +0100

    refactoring: correzione esercizio con ciclo for

--- js/script.js
+++ js/script.js
@@ -12,9 +12,16 @@ createApp({
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then ((resp) => {
                     console.log(resp);
-                    do {
-                        this.randomEmail.push(resp.data.response)
-                    } while (this.randomEmail.length > 10);
+                    // do {
+                    //     this.randomEmail.push(resp.data.response)
+                    // } while (this.randomEmail.length > 10);
+
+                   if (this.randomEmail.length < 10) {
+                    this.randomEmail.push(resp.data.response)
+                   }else{
+                    this.randomEmail.length = 0
+                   }
+                   
                 })
                 
         },
