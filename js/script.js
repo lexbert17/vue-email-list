899c38665acdefc167d071f9d29c9820e5b71e7d
lexbert17 <lexbertlazo@gmail.com>
2023-11-21 14:34:47 +0100

    add: creazione di 10 email random

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            randomEmail:[],
         }
     },
     methods: {
-        
+        generateEmail(){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then ((resp) => {
+                    console.log(resp);
+                    this.randomEmail.push(resp.data.response)
+                    if (this.randomEmail.length == 11) {
+                        this.randomEmail.length = 0
+                    }
+                })
+                
+        },
     }
 }).mount("#app")
+
